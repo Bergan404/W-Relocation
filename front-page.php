f154d67e5d44012efba7fff950545cfdcf6ec9e8
Bergan Oudshoorn <bergan404@gmail.com>
2023-10-16 10:25:20 -0400

    starting on the next section

--- front-page.php
+++ front-page.php
@@ -34,19 +34,19 @@ get_header(); ?>
         <div class="row py-5 justify-content-between">
             <div class="col text-center">
                 <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/computer.svg" alt="">
-                <h4 class="py-4 gray">TECHNOLOGY.</h4>
+                <h5 class="py-4 gray">TECHNOLOGY.</h5>
             </div>
             <div class="col text-center">
                 <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/Hands.svg" alt="">
-                <h4 class="py-4 purple">CARING.</h4>
+                <h5 class="py-4 purple">CARING.</h5>
             </div>
             <div class="col text-center">
                 <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/People.svg" alt="">
-                <h4 class="py-4 blue">TEAM.</h4>
+                <h5 class="py-4 blue">TEAM.</h5>
             </div>
             <div class="col text-center">
                 <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/Wizard.svg" alt="">
-                <h4 class="py-4 brown">FULL-SERVICE.</h4>
+                <h5 class="py-4 brown">FULL-SERVICE.</h5>
             </div>
         </div>
     </div>
@@ -57,9 +57,19 @@ get_header(); ?>
         <div class="col">
 
         </div>
-        <div class="col">
-            <h3>DISCOVER W RELOCATION</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <div class="col white-txt">
+            <h3><em>DISCOVER W RELOCATION</em></h3>
+            <p>W Relocation specializes in offering benefits and personalized
+                discounts for your members/employees. We ensure that these
+                benefits are customized to fit every person's needs, from Real Estate,
+                Mortgages, and Title Services, making sure these big life events are
+                an enjoyable experience.</p>
+            <p>Our program was designed to allow you to offer valuable and
+                impactful benefits and discounts to your employees, which leads to
+                high morale, retention, and appeal to new talent. </p>
+            <a href="/">
+                <button class="discover-btn">Learn More</button>
+            </a>
         </div>
     </div>
 </section>
