59b3ac7da6116030e79135f24e3c5bcb5fabfbfc
Bergan Oudshoorn <bergan404@gmail.com>
2023-10-16 11:22:16 -0400

    have to get back to work

--- front-page.php
+++ front-page.php
@@ -33,31 +33,31 @@ get_header(); ?>
         <h3 class="text-center line-above">WHY W RELOCATION</h3>
         <div class="row py-5 justify-content-between">
             <div class="col text-center">
-                <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/computer.svg" alt="">
+                <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/computer.svg" alt="Technology">
                 <h5 class="py-4 gray">TECHNOLOGY.</h5>
             </div>
             <div class="col text-center">
-                <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/Hands.svg" alt="">
+                <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/Hands.svg" alt="Caring">
                 <h5 class="py-4 purple">CARING.</h5>
             </div>
             <div class="col text-center">
-                <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/People.svg" alt="">
+                <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/People.svg" alt="Team">
                 <h5 class="py-4 blue">TEAM.</h5>
             </div>
             <div class="col text-center">
-                <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/Wizard.svg" alt="">
+                <img class="svg-icon" src="<?php echo get_theme_file_uri() ?>/assets/img/Wizard.svg" alt="Full Service">
                 <h5 class="py-4 brown">FULL-SERVICE.</h5>
             </div>
         </div>
     </div>
 </section>
 
-<section class="container-fluid py-5 gray-bg">
-    <div class="row">
+<section class="container-fluid py-6 gray-bg px-0">
+    <div class="row align-items-center">
         <div class="col">
-
+            <img src="<?php echo get_theme_file_uri() ?>/assets/img/discover-image.webp" alt="Discover Interior">
         </div>
-        <div class="col white-txt">
+        <div class="col white-txt px-5">
             <h3><em>DISCOVER W RELOCATION</em></h3>
             <p>W Relocation specializes in offering benefits and personalized
                 discounts for your members/employees. We ensure that these
